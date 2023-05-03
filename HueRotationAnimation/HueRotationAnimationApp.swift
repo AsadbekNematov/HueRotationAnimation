38d575f54a293882d0383c13110f903e31ea22ad
Asadbek <asadbekn443@gmail.com>
2023-05-03 04:23:11 -0400

    Initial Commit

---
+++ HueRotationAnimation/HueRotationAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueRotationAnimationApp.swift
+//  HueRotationAnimation
+//
+//  Created by Asadbek Nematov on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HueRotationAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
